6d0fe58690dfd602fdd3cc578c648f20b0a9b455
GuangLiu <godfatherlg@outlook.com>
2026-02-18 01:33:58 +0800

    fix: capitalize Wikilinks and Markdown properly

--- main.ts
+++ main.ts
@@ -1836,8 +1836,8 @@ class LinkerSettingTab extends PluginSettingTab {
         if (!this.plugin.settings.useDefaultLinkStyleForConversion) {
             // Toggle setting to use markdown links
             new Setting(containerEl)
-                .setName('Use [[wiki-links]]')
-                .setDesc('If toggled, the virtual links will be created as wiki-links instead of markdown links.')
+                .setName('Use [[Wikilinks]]')
+                .setDesc('If toggled, the virtual links will be created as Wikilinks instead of Markdown links.')
                 .addToggle((toggle) =>
                     toggle.setValue(!this.plugin.settings.useMarkdownLinks).onChange(async (value) => {
                         await this.plugin.updateSettings({ useMarkdownLinks: !value });
