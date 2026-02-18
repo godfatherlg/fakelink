44dbdc06f367942332600166260f693ab320f8df
GuangLiu <godfatherlg@outlook.com>
2026-02-18 15:05:33 +0800

    fix: change 'preview windows' to 'Reading view' (current terminology)

--- main.ts
+++ main.ts
@@ -1717,7 +1717,7 @@ class LinkerSettingTab extends PluginSettingTab {
             // Toggle setting to exclude links to the active file
             new Setting(containerEl)
                 .setName('Exclude self-links to the current note')
-                .setDesc('If toggled, links to the note itself are excluded from the linker. (This might not work in preview windows.)')
+                .setDesc('If toggled, links to the note itself are excluded from the linker. (This might not work in Reading view.)')
                 .addToggle((toggle) =>
                     toggle.setValue(this.plugin.settings.excludeLinksToOwnNote).onChange(async (value) => {
                         await this.plugin.updateSettings({ excludeLinksToOwnNote: value });
